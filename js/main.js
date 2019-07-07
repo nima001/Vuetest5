f87bb22ea81ceb819c2f48f23a3c46a6aab4b737
nima001 <1994974597@qq.com>
2019-07-07 15:50:09 +0800

    '自定义指令加入修饰符和指令'

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 
 Vue.directive('pin',function (el,val) {
     var pined = val.value;
+    var position = val.modifiers;//修饰符
+    var arg = val.arg;//参数
+
+
     console.log("pined",pined);
     if(pined){
+        for(var key in position){
+            if(position[key]){
+               el.style[key] ='100px';
+            }
+        }
+        if(arg === 'true'){
+            el.style.backgroundColor='yellow';
+        }
         el.style.position='fixed';
-        el.style.top='200px';
     }else{
+        el.style.backgroundColor='darkgray';
         el.style.position='static';
         el.style.top='100px';
     }
